3343b95db595ba806439fa9aa2ae2d4aaaeefb56
mazurro1 <mazul96.hm@gmail.com>
2019-06-17 13:59:28 +0200

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style/App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    nickname: 'nick',
+    email: 'em',
+    ipAdress: 'ip',
+    allUsers: [
+      {
+        id: 0,
+        nickname: 'Hubert',
+        email: 'mazul96.hm@gmail.com',
+        ipAdress: '123.456.789'
+      },
+      {
+        id: 1,
+        nickname: 'Milena',
+        email: 'mazul96.hm@gmail.com',
+        ipAdress: '123.456.789'
+      },
+    ]
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <FormIp
+          nickname={this.state.nickname}
+          email={this.state.email}
+          ipAdress={this.state.ipAdress}
+          handleChange={this.handleChange}
+        />
+        {CreateTable(this.state.allUsers)}
+      </div>
+    );
+  }
 }
 
-export default App;
+
+const FormIp = props => {
+  const { nickname, email, ipAdress, handleChange } = props;
+  return (
+    <>
+      <form action="">
+        <input type="text" name={'nickname'} value={nickname} onChange={handleChange} /><br />
+        <input type="text" name={'email'} value={email} onChange={handleChange} /><br />
+        <input type="text" name={'ipAdress'} value={ipAdress} onChange={handleChange} /><br />
+        <button>Add user</button>
+      </form>
+    </>
+  )
+}
+
+const CreateTable = allUsers => {
+  const users = allUsers.map(user =>
+    <CreateUser
+      key={user.id}
+      nickname={user.nickname}
+      email={user.email}
+      ipAdress={user.ipAdress}
+    />
+  )
+  return users;
+}
+
+const CreateUser = props => {
+  const { nickname, email, ipAdress } = props;
+
+  return (
+    <div className='row'>
+      <div className='col-4'>
+        {nickname}
+      </div>
+      <div className='col-4'>
+        {email}
+      </div>
+      <div className='col-4'>
+        {ipAdress}
+      </div>
+    </div>
+  )
+}
