0fea6c86ddbbdb15e6ad6ce5ac81ca396743c83e
mazurro1 <mazul96.hm@gmail.com>
2019-06-18 15:19:08 +0200

    Updates

---
+++ precache-manifest.9e17b926cf1dda48927c036ae434479d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6082aa84a3a50649a3b282c259b7597c",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "50a73ba7abbe946cc5cb",
+    "url": "/todoApp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "18187d4c25b200c1786e",
+    "url": "/todoApp/static/css/main.1e111450.chunk.css"
+  },
+  {
+    "revision": "50a73ba7abbe946cc5cb",
+    "url": "/todoApp/static/js/2.b4b6e5cc.chunk.js"
+  },
+  {
+    "revision": "18187d4c25b200c1786e",
+    "url": "/todoApp/static/js/main.2e924685.chunk.js"
+  },
+  {
+    "revision": "7ffb180d2dcaa0b59a98",
+    "url": "/todoApp/static/js/runtime~main.1d50937c.js"
+  }
+]);
